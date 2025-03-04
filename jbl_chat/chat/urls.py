6ea1cab2d919f2f81fe0d0a5c0278af33a23f864
Dimmanzo <dmitrij.sazniov@gmail.com>
2025-03-04 23:23:36 +0100

    Added an endpoint to get all users and linked chat URLs in the main config.

---
+++ jbl_chat/chat/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import get_all_users
+
+
+urlpatterns = [
+    path('users/', get_all_users, name='get_all_users'),
+]
--- jbl_chat/chat/views.py
+++ jbl_chat/chat/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import JsonResponse
 
-# Create your views here.
+
+# API endpoint that returns a list of all users.
+def get_all_users(request):
+    # Gets only id's and usernames.
+    user_data = User.objects.values("id", "username")
+    return JsonResponse(list(user_data), safe=False)
